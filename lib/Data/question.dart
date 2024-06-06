e51e5d287a793aef6607043ea540171bda1ba11c
MMETehrani <mmtehrani36@gmail.com>
2024-06-06 16:41:51 +0330

    add list questions

---
+++ lib/Data/question.dart
@@ -0,0 +1,6 @@
+class question {
+  String? qTitle;
+  String? imageNumber;
+  List<String>? answerList;
+  int? correctAnswer;
+}
---
+++ lib/constants/constants.dart
@@ -0,0 +1,22 @@
+import 'package:quizz_app/Data/question.dart';
+
+List<question> getQuestionList() {
+  var firstQuestion = question();
+  firstQuestion.qTitle = 'مشهور ترین شعبده‌باز جهان کیست؟';
+  firstQuestion.imageNumber = '1';
+  firstQuestion.correctAnswer = 2;
+  firstQuestion.answerList = [
+    'امیراحمدادیبی',
+    'دیوید کاپرفیلد',
+    'هری هودینی',
+    'دیوید بلین'
+  ];
+
+  var secondQuestion = question();
+  secondQuestion.qTitle = 'چه کشوری ایستگاه فضایی میر را بنا کرد؟';
+  secondQuestion.imageNumber = '2';
+  secondQuestion.correctAnswer = 0;
+  secondQuestion.answerList = ['(شوروی سابق)روسیه', 'آمریکا', 'چین', 'هند'];
+
+  return [firstQuestion, secondQuestion];
+}
